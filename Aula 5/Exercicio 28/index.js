ff917a229d02be354461b71e117f7dc3aba4a334
Francisco <chicomazfilho@hotmail.com>
2019-09-08 20:45:12 -0300

    Remove pacotes desnecessários

--- Aula 5/Exercicio 28/index.js
+++
@@ -1,6 +0,0 @@
-const level = require('./node_modules/level')
-const db = level('palavras')
-
-db.put('palavras', process.argv, { valueEncoding: "json" })
-
-db.get('palavras', (error, content) => console.log(content))
